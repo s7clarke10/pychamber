83a42b2c89da74d9e14a8ea60ff78314c7e8d807
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2023-10-07 16:34:31 +1300

    Updating description.

--- pychamber/utils/execution_utils.py
+++ pychamber/utils/execution_utils.py
@@ -5,7 +5,7 @@ import subprocess
 
 def run_command(command, env):
     """ Runs requested process with arguments.
-        Enable debug logging and raise errors.
+        Return: returncode of executed program.
     """
     logging.debug("Command: {}".format(command))
     result = subprocess.run(command, env=env, shell=False, capture_output=False)
